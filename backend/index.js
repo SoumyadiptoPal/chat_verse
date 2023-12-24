ab4f8530396a1db86a526901b0d2909bd0b0e387
SoumyadiptoPal <soumyadiptocodes@gmail.com>
2023-12-25 00:14:19 +0530

    restored prev commit

--- backend/index.js
+++ backend/index.js
@@ -18,45 +18,45 @@ const server=app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-// const io = require("socket.io")(server, {
-//   pingTimeout: 60000,
-//   cors: {
-//     origin: "http://localhost:3000",
-//     // credentials: true,
-//   },
-// });
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+    // credentials: true,
+  },
+});
 
-// io.on("connection", (socket) => {
-//   // console.log("Connected to socket.io");
-//   socket.on("setup", (userData) => {
-//     socket.join(userData._id);
-//     socket.emit("connected");
-//   });
+io.on("connection", (socket) => {
+  // console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
 
-//   socket.on("join chat", (room) => {
-//     socket.join(room);
-//     console.log("User Joined Room: " + room);
-//   });
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
 
-//   socket.on("new message", (newMessageRecieved) => {
-//     var chat = newMessageRecieved.chat;
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
 
-//     if (!chat.users) return;
-//     // -----------------
-//     // console.log("chat.users not defined");
+    if (!chat.users) return;
+    // -----------------
+    // console.log("chat.users not defined");
 
-//     // chat.users.forEach((user) => {
-//     //   if (user._id == newMessageRecieved.sender._id) return;
-//     //   console.log(user);
-//     //   socket.in(user._id).emit("message recieved", newMessageRecieved);
-//     // });
-//     // -----------------------
-//     io.emit('message recieved', newMessageRecieved);
-//   });
+    // chat.users.forEach((user) => {
+    //   if (user._id == newMessageRecieved.sender._id) return;
+    //   console.log(user);
+    //   socket.in(user._id).emit("message recieved", newMessageRecieved);
+    // });
+    // -----------------------
+    io.emit('message recieved', newMessageRecieved);
+  });
   
-//   socket.off("setup", () => {
-//     console.log("USER DISCONNECTED");
-//     socket.leave(userData._id);
-//   });
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
 
-// })
+})
